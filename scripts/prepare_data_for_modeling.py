cb14ec0106d7832c1fd20c7f1c016b433cba32bb
Eduardo Sanchez <ed.rasanmar@gmail.com>
2025-12-06 12:05:57 -0800

    Data Validation Completed

--- scripts/prepare_data_for_modeling.py
+++ scripts/prepare_data_for_modeling.py
@@ -17,6 +17,58 @@ def create_dir_if_not_exists(directory):
 def main(input_dir, out_dir):
     create_dir_if_not_exists(out_dir)
 
+    # Load the data 
+    cleveland_data = pd.read_csv(input_dir)
+
+    # Remove rows with '?' in ca or thal
+    cleveland_data = cleveland_data[~cleveland_data["ca"].astype(str).str.contains(r"\?")]
+    cleveland_data = cleveland_data[~cleveland_data["thal"].astype(str).str.contains(r"\?")]
+
+    #Data Validation
+
+    schema = pa.DataFrameSchema( #Check Correct Colum Names, and Data Types
+        {
+            "age": pa.Column(int, pa.Check.between(18, 100),nullable=True,coerce=True), #No Outliers -> Check.between
+            "sex": pa.Column(int, pa.Check.isin([0, 1]),nullable=True,coerce=True),  #Correct category levels -> Check.isin
+            "cp": pa.Column(int, pa.Check.isin([1, 2, 3, 4]),nullable=True,coerce=True), #Missingness OK -> nullable= True 
+            "trestbps": pa.Column(int,nullable=True,coerce=True),
+            "chol": pa.Column(int,nullable=True,coerce=True),
+            "fbs": pa.Column(int, pa.Check.isin([0, 1]),nullable=True,coerce=True),
+            "restecg":  pa.Column(int, pa.Check.isin([0, 1, 2]), nullable=True,coerce=True),
+            "thalach": pa.Column(int,nullable=True,coerce=True),
+            "exang": pa.Column(int, pa.Check.isin([0, 1]),nullable=True,coerce=True),
+            "oldpeak": pa.Column(float,nullable=True, coerce=True),
+            "slope": pa.Column(int, pa.Check.isin([1, 2, 3]),nullable=True,coerce=True),
+            "ca": pa.Column(float, pa.Check.isin([0, 1, 2, 3]),nullable=True,coerce=True),
+            "thal": pa.Column(float,nullable=True, coerce=True),
+            "target": pa.Column(int, pa.Check.isin([0, 1, 2, 3, 4]),nullable=True,coerce=True) #Target/response variable follows expected distribution
+        },
+        checks=[
+            pa.Check(lambda df: ~df.duplicated().any(), error="Duplicate rows found."),  # No Duplicate Observations
+            pa.Check(lambda df: ~(df.isna().all(axis=1)).any(), error="Empty rows found.") # No empty observations
+
+        ]
+    )
+
+    cleveland_data = schema.validate(cleveland_data)#Validated Correct file format
+
+    heart_train_ds = Dataset(cleveland_data, label="target", cat_features=["sex", "cp", "fbs", "restecg", "exang", "slope", "ca", "thal"])
+
+    #Target-Feature Correlation Check - No anomalous target–feature correlations
+    check_feat_lab_corr = FeatureLabelCorrelation().add_condition_feature_pps_less_than(0.6)
+    check_feat_lab_corr_result = check_feat_lab_corr.run(dataset=heart_train_ds)
+
+    if not check_feat_lab_corr_result.passed_conditions():
+        raise ValueError("Feature-Label correlation exceeds the maximum acceptable threshold.")
+
+    #Feature-Feature Correlation Check - No anomalous feature–feature correlations
+    check_feat_feat_corr = FeatureFeatureCorrelation().add_condition_max_number_of_pairs_above_threshold(0.6)
+    check_feat_feat_corr_result = check_feat_feat_corr.run(dataset=heart_train_ds)
+
+    if not check_feat_feat_corr_result.passed_conditions():
+        raise ValueError("Feature-Feature correlation exceeds the maximum acceptable threshold.")
+    
+
 
 if __name__ == '__main__':
     main()
